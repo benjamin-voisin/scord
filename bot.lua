5dd98a97ba7d5d0cecb3b3fdabad10d36a99a382
Benjamin V <benjamin.voisin@ens-rennes.fr>
2024-01-25 14:33:32 +0100

    ✨ now cri

--- bot.lua
+++ bot.lua
@@ -1,8 +1,34 @@
 local discordia = require('discordia')
 local client = discordia.Client()
 
+local function cri(message)
+  local prefix = {"cri", "cry", "Cri", "Cry", "CRI", "CRY"}
+  local j = nil
+  for k=1, #prefix do
+    local _, new_j = string.find(message, prefix[k])
+    if new_j then
+      if j then
+        if new_j < j then
+          j = new_j
+        end
+      else
+        j = new_j
+      end
+    end
+  end
+  if j then
+    return string.upper(string.sub(message, j + 1, #message))
+  else
+    return nil
+  end
+end
+
 local function di(message)
-  local prefix = {"di", "dy", "d i", "d y", "dhi", "dhy"}
+  local message_cri = cri(message)
+  if (message_cri) then
+    return message_cri
+  end
+  local prefix = {"di", "dy", "d i", "d y", "dhi", "dhy", "Di", "Dy", "DI", "DY"}
   local j = nil
   for k=1, #prefix do
     local _, new_j = string.find(message, prefix[k])
@@ -30,7 +56,7 @@ end)
 client:on('messageCreate', function(message)
   print(message.content)
   local response = di(message.content)
-  if response and (response ~= '') then
+  if response and (response ~= '') and (math.random() < 0.5) then
 		-- message.channel:send('<@'..message.author.id..'> '..response)
 		message.channel:send(response)
 	end
